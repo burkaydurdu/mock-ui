e7ce23e071c2d72c3585d7920eabb549a867d04e
burkaydurdu <burkaydurdu@outlook.com>
2021-09-03 00:09:10 +0300

    Added github links

--- src/mock_ui/navigation_views.cljs
+++ src/mock_ui/navigation_views.cljs
@@ -13,11 +13,19 @@
        [:h1.text-white.font-bold.text-2xl
         [:span "Puppet"]
         [:span.text-green-500 "API"]]]
-      (when current-user
-        [:div.text-white
-         [dropdown
-          {:options [{:title "Logout"
-                      :class "hover:text-red-500"
-                      :on-click-fn #(dispatch [::events/logout])}]
-           :class   "text-white"
-           :title   (:name current-user)}]])]]))
+      [:div.flex.items-center
+       [:a.fab.fa-github.text-white.text-2xl.mr-3
+        {:href   "https://github.com/burkaydurdu/mock-ui"
+         :target "__blank"}]
+       [:a.text-white
+        {:href   "https://github.com/burkaydurdu/mock-ui/issues/new"
+         :target "__blank"}
+        "Report"]
+       (when current-user
+         [:div.text-white.ml-3
+          [dropdown
+           {:options [{:title "Logout"
+                       :class "hover:text-red-500"
+                       :on-click-fn #(dispatch [::events/logout])}]
+            :class   "text-white"
+            :title   (:name current-user)}]])]]]))
